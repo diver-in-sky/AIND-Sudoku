12e119dbb85b798d966a82c09616a6f8f1423eae
Andrey Tereskin <tereskin@gmail.com>
2017-02-18 15:08:53 +0900

    Sudoku naked_twins and diagonal solution

--- solution.py
+++ solution.py
@@ -1,12 +1,30 @@
 assignments = []
 
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [a+b for a in A for b in B]
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diag_units = [[r+c for r,c in zip(rows,cols)], [r+c for r,c in zip(rows, cols[::-1])]]
+unitlist = row_units + column_units + square_units + diag_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+digit_pairs = [dig1+dig2 for dig1 in '123456789' for dig2 in '123456789' if dig2 > dig1]
+
+
 def assign_value(values, box, value):
     """
     Please use this function to update your values dictionary!
     Assigns a value to a given box. If it updates the board record it.
     """
+    prev_value = values[box]
     values[box] = value
-    if len(value) == 1:
+    if len(value) == 1 and prev_value!=value:
         assignments.append(values.copy())
     return values
 
@@ -21,10 +39,15 @@ def naked_twins(values):
 
     # Find all instances of naked twins
     # Eliminate the naked twins as possibilities for their peers
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    for unit in unitlist:
+        for pair in digit_pairs:
+            twins = [box for box in unit if values[box]==pair]
+            if len(twins) == 2:
+                for box in unit:
+                    if not box in twins:
+                        new_value = values[box].replace(pair[0], '').replace(pair[1], '')
+                        assign_value(values, box, new_value)
+    return values
 
 def grid_values(grid):
     """
@@ -36,7 +59,7 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    return dict((k, v if v!='.' else '123456789') for k, v in zip(boxes, grid))
 
 def display(values):
     """
@@ -44,16 +67,66 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    """Eliminate values from peers of each box with a single value.
+
+    Go through all the boxes, and whenever there is a box with a single value,
+    eliminate this value from the set of values of all its peers.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after eliminating values.
+    """
+    single_value = [(b, v) for b, v in values.items() if len(v)==1]
+    for box, v in single_value:
+        for peer in peers[box]:
+            assign_value(values, peer, values[peer].replace(v, ''))
+    return values
 
 def only_choice(values):
-    pass
+    """Finalize all values that are the only choice for a unit.
+
+    Go through all the units, and whenever there is a unit with a value
+    that only fits in one box, assign the value to this box.
+
+    Input: Sudoku in dictionary form.
+    Output: Resulting Sudoku in dictionary form after filling in only choices.
+    """
+    for unit in unitlist:
+        for val in '123456789':
+            b = [box for box in unit if val in values[box]]
+            if len(b) == 1:
+                assign_value(values, b[0], val)
+    return values
 
 def reduce_puzzle(values):
-    pass
+    stalled = False
+    while not stalled:
+        # Check how many boxes have a determined value
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+
+        # Eliminate Strategy
+        values = eliminate(values)
+        # Only Choice Strategy
+        values = only_choice(values)
+        # Naked Twins Strategy
+        values = naked_twins(values)
+
+        # Check how many boxes have a determined value, to compare
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        # If no new values were added, stop the loop.
+        stalled = solved_values_before == solved_values_after
+        # Sanity check, return False if there is a box with zero available values:
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
 
 def search(values):
     pass
@@ -67,10 +140,38 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    values=grid_values(grid)
+
+    # First, reduce the puzzle using the previous function
+    values = reduce_puzzle(values)
+    if not values:
+        return False
+
+    # Choose one of the unfilled squares with the fewest possibilities
+    counts = [(b, len(values[b])) for b in boxes if len(values[b]) > 1]
+    if len(counts) == 0:
+        return values
+
+    b_min, _ = min(counts, key=lambda x: x[1])
+
+    # Now use recursion to solve each one of the resulting sudokus, and if one returns a value (not False), return that answer!
+    for v in values[b_min]:
+        new_values = values.copy()
+        assign_value(new_values, b_min, v)
+        res = search(new_values)
+        if res:
+            return res
+
+    return False
+
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
     display(solve(diag_sudoku_grid))
+
+    # for assign in assignments:
+    #     print("\n\n")
+    #     display(assign)
 
     try:
         from visualize import visualize_assignments
